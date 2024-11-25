13ec94f5f635e86078cdccba7ae2fefbe91b0c18
Sharp Franklin <sharp.franklin@weevilcorp.com>
2024-11-25 00:15:30 +0000

    second commit

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,18 +1,40 @@
+
+import img1 from '../images/logo.png';
+import me1 from '../images/me_lol.jpg';
+import bitcoin from '../images/bitcoin-landscape.png';
+
 import './index.scss';
 
 export default function Index() {
+  const handleClick = () => {
+    const link = document.createElement('a');
+    link.href = 'downloads/Stuart Bradbury - Curriculum Vitae.docx'; // Replace with the URL of your file
+    link.download = 'Stuart Bradbury - Curriculum Vitae.docx'; // Replace with the desired file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const visitLivestream = () => {
+    window.open('/about', "_self");
+  }
+
+  const visitGithub = () => {
+    window.open('https://github.com/sbrdbry/the-app', "_blank");
+  }
+
   return (
     <>
 <div className="container">
   <div className="row">
     <div className="column-66">
-      <h1 className="xlarge-font"><b>The App</b></h1>
-      <h1 className="large-font" style={{color: "MediumSeaGreen"}}><b>Why buy it?</b></h1>
-      <p><span style={{fontSize: "36px"}}>Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <button className="button">Download Application</button>
+      <h1 className="xlarge-font"><b>Stuart Bradbury</b></h1>
+      <h1 className="large-font" style={{color: "MediumSeaGreen"}}><b>Who am I?</b></h1>
+      <p><span style={{fontSize: "36px"}}>Develop software like a pro.</span> You should hire me because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <button className="button" onClick={handleClick}>Download C.V.</button>
     </div>
     <div className="column-33">
-        <img src="https://www.w3schools.com/w3images/img_app.jpg" width="335" height="471"/>
+        <img src={img1} width="335" height="471"/>
     </div>
   </div>
 </div>
@@ -20,13 +42,13 @@ export default function Index() {
 <div className="container" style={{backgroundColor:"#f1f1f1"}}>
   <div className="row">
     <div className="column-33">
-      <img src="https://www.w3schools.com/w3images/app5.jpg" alt="App" width="335" height="471"/>
+      <img src={me1} alt="App" width="335" height="471"/>
     </div>
     <div className="column-66">
-      <h1 className="xlarge-font"><b>Clarity</b></h1>
-      <h1 className="large-font" style={{color:"red"}}><b>Pixels, who?</b></h1>
-      <p><span style={{fontSize:"24px"}}>A revolution in resolution.</span> Sharp and clear photos with the world's best photo engine, incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-      <button className="button" style={{backgroundColor:"red"}}>Read More</button>
+      <h1 className="xlarge-font"><b>Eco</b></h1>
+      <h1 className="large-font" style={{color:"#F4511E"}}><b>Much, how?</b></h1>
+      <p><span style={{fontSize:"24px"}}>From just 0.00013BTC an hour.</span> Sharp and clean solutions with the world's best developer, incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      <button className="button" style={{backgroundColor:"#F4511E"}} onClick={visitLivestream}>Visit Livestream</button>
     </div>
   </div>
 </div>
@@ -34,13 +56,13 @@ export default function Index() {
 <div className="container">
   <div className="row">
     <div className="column-66">
-      <h1 className="xlarge-font"><b>The App</b></h1>
-      <h1 className="large-font" style={{color:"MediumSeaGreen"}}><b>Why buy it?</b></h1>
-      <p><span style={{fontSize:"36px"}}>Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-      <button className="button">Download Application</button>
+      <h1 className="xlarge-font"><b>Money</b></h1>
+      <h1 className="large-font" style={{color:"#F7931A"}}><b>How to pay?</b></h1>
+      <p><span style={{fontSize:"36px"}}>Send us some Bitcoin.</span> Contact me for a personal Bitcoin address, and let us know what it is you want in return ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      <button className="button" style={{backgroundColor:"#F7931A"}} onClick={visitGithub}>GitHub Repo</button>
     </div>
     <div className="column-33">
-        <img src="https://www.w3schools.com/w3images/img_app.jpg" width="335" height="471" />
+        <img src={bitcoin} width="335" height="471" />
     </div>
   </div>
 </div>
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -8,6 +8,8 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 import './root.scss';
 
 export default function Root() {
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
   const [ toggled, setToggled ] = useState(false);
   const [ navHeight, setNavHeight ] = useState(0);
 
@@ -33,23 +35,36 @@ export default function Root() {
     }
   }, []);
 
+  const handleScroll = () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollPercent = (scrollTop / scrollHeight) * 100;
+    setScrollPercentage(scrollPercent);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
-      <div className={toggled ? "topnav responsive" : "topnav"} id="myTopnav">
+      <div className={toggled ? "topnav responsive" : "topnav"} style={{opacity: "0.8"}} id="myTopnav">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/news">News</NavLink>
         <NavLink to="/about">livestream</NavLink>
         <a href="#" className="icon" onClick={(event) => myFunction(event)}>
           <FontAwesomeIcon icon={faBars} />
-        </a>
+        </a>        
       </div>
+      <div className="scroll-progress" style={{backgroundImage: `linear-gradient(to right, orange ${scrollPercentage}%, rgba(0,0,0,0) ${scrollPercentage}%)`}}></div>
 
       <main id="main" style={{marginTop: navHeight}}>
         <Outlet />
       </main>
 
       <div className="footer">
-        <h2>Footer</h2>
+        <h2><a style={{fontSize: "14px"}} href="bitcoin:bc1qae66ucqsf5nkrxwca7z93rzd8dh95793rk0f3r">bc1qae66ucqsf5nkrxwca7z93rzd8dh95793rk0f3r</a></h2>
       </div>
     </>
   )
